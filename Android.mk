a90fc41c205714e531590d8902ac81d93e4102fd
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-08-30 16:50:18 +0200

    Revert "bt: Add guard makefile"
    This reverts commit dedf818255f5a19bcfb3b2ff76a17bb89a37bb2a.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,3 @@
-BT_VENDOR_PATH := $(call my-dir)
-
-ifeq ($(BT_VENDOR_PATH),$(call project-path-for,bt-vendor))
-
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
@@ -20,6 +16,4 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       endif
     endif
   endif
-endif
-
 endif
